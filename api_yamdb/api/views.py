aa71bc0e84cd7a20038dcb3a9d97f893f98a0435
ikhit <furyheart@mail.ru>
2024-01-25 18:45:16 +0300

    test users urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import filters
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
 
 from .serializers import UserSerializer
 from reviews.models import User
@@ -9,8 +8,5 @@ from reviews.models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [
-    #     IsAdminUser,
-    # ]
     filter_backends = filters.SearchFilter
     search_fields = ('username',)
