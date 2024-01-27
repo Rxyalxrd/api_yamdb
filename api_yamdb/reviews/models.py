cb84b14fe961b86465e105cb5b4bf7fdf2628516
Maxim Tsaregradtsev <3m3rcy3@gmail.com>
2024-01-27 16:18:35 +0300

    add Comments and Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth import get_user_model
 
 from .validators import validate_year
@@ -88,3 +89,83 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """Модель отзывов."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='произведение'
+    )
+    text = models.TextField(
+        max_length=1024,
+        null=False,
+        verbose_name="Текст отзыва.",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name='Автор отзыва.',
+    )
+    score = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name="Оценка от 1 до 10.",
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации.',
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_title_author'
+            )
+        ]
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(models.Model):
+    """Модель комментариев."""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв.'
+    )
+    text = models.TextField(
+        max_length=1024,
+        verbose_name="Текст комментария.",
+        null=False
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        verbose_name='Автор комментария.',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации.',
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['review', 'author'],
+                name='unique_review_author'
+            )
+        ]
+
+    def __str__(self):
+        return self.text
